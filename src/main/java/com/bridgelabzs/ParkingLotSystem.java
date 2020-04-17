21b1b6b95a6c002a45b0b5705eeb35b0730c9778
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-17 18:06:16 +0530

    REFACTORED:Code To Throw Exception And Find Size Of List

--- src/main/java/com/bridgelabzs/ParkingLotSystem.java
+++ src/main/java/com/bridgelabzs/ParkingLotSystem.java
@@ -1,23 +1,29 @@
 package com.bridgelabzs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotSystem {
-    private Object vehicle;
+    List<Object> vehicle = new ArrayList<Object>();
 
     //METHOD TO PARK THE VEHICLE
-    public boolean isPark(Object vehicle) {
-        if (this.vehicle != null)
-            return false;
-        this.vehicle = vehicle;
-        return true;
+    public boolean isPark(Object vehicleToPark) throws ParkingLotSystemException {
+        if (this.vehicle.isEmpty()) {
+            vehicle.add(vehicleToPark);
+            return true;
+        } else if (!this.vehicle.isEmpty() && (!this.vehicle.contains(vehicleToPark)) && (this.vehicle.size() < 5)) {
+            vehicle.add(vehicleToPark);
+            return true;
+        } else
+            throw new ParkingLotSystemException("Parking Lot Is Full");
     }
 
     //METHOD TO UNPARK THE GIVEN VEHICLE
-    public boolean unParkTheVehicle(Object vehicle) {
-        this.vehicle = vehicle;
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+    public boolean unParkTheVehicle(Object vehicleToUnPark) throws ParkingLotSystemException {
+        if (vehicle.contains(vehicleToUnPark)) {
+            vehicle.remove(vehicleToUnPark);
             return true;
         }
-        return false;
+        throw new ParkingLotSystemException("Vehicle Is Not Parked Here");
     }
 }
