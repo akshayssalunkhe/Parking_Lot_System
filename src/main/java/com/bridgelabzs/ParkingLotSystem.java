c0d4fe9df87388d4495cf5615c9203a5abcbf3e9
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-18 17:24:21 +0530

    REFACTORED:Code As Per The Requirment Of Observer Pattern

--- src/main/java/com/bridgelabzs/ParkingLotSystem.java
+++ src/main/java/com/bridgelabzs/ParkingLotSystem.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLotSystem {
+
     List<Object> vehicle = new ArrayList<Object>();
 
     //VARIABLE
     int parkingLotCapacity = 5;
+    public boolean isParkingFull;
+    ParkingLotInformationSubscriber parkingLotInformationSubscriber;
+
+    //CONSTRUCTOR
+    public ParkingLotSystem() {
+        parkingLotInformationSubscriber = new ParkingLotInformationSubscriber();
+    }
 
     //METHOD TO PARK THE VEHICLE
     public boolean isPark(Object vehicleToPark) throws ParkingLotSystemException {
@@ -17,16 +25,18 @@ public class ParkingLotSystem {
         } else if (!this.vehicle.isEmpty() && (!this.vehicle.contains(vehicleToPark)) && (this.vehicle.size() < parkingLotCapacity)) {
             vehicle.add(vehicleToPark);
             return true;
-        } else
-            throw new ParkingLotSystemException("Parking Lot Is Full");
+        } else isParkingFull = true;
+        parkingLotInformationSubscriber.notifyParkingStatus(true);
+        return true;
     }
 
     //METHOD TO UNPARK THE GIVEN VEHICLE
     public boolean unParkTheVehicle(Object vehicleToUnPark) throws ParkingLotSystemException {
         if (vehicle.contains(vehicleToUnPark)) {
             vehicle.remove(vehicleToUnPark);
+            parkingLotInformationSubscriber.notifyParkingStatus(false);
             return true;
         }
-        throw new ParkingLotSystemException("Vehicle Is Not Parked Here");
+        throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.VEHICLE_NOT_PARK_HERE, "Vehicle Is Not Parked Here");
     }
 }
