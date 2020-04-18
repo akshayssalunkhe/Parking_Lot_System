aa48f4f6be4dd80aff46f52759ddade2b08c986c
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-18 17:25:16 +0530

    ADDED:Enum To Exception Class

--- src/main/java/com/bridgelabzs/ParkingLotSystemException.java
+++ src/main/java/com/bridgelabzs/ParkingLotSystemException.java
@@ -1,10 +1,15 @@
 package com.bridgelabzs;
 
 public class ParkingLotSystemException extends Exception {
+    //ENUM
+    public enum ExceptionType {VEHICLE_NOT_PARK_HERE, PARKING_LOT_IS_FULL};
+
+    public ExceptionType type;
     private final String message;
 
     //CONSTRUCTOR
-    public ParkingLotSystemException(String message) {
+    public ParkingLotSystemException(ExceptionType type, String message) {
+        super(message);
         this.message = message;
     }
 }
