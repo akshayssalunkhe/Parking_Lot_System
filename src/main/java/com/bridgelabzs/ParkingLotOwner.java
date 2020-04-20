c1f0c87eb1298225afdc06df2e7cf6895bb3c859
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-20 11:33:05 +0530

    ADDED:Method To Generate Parking Lot Number

--- src/main/java/com/bridgelabzs/ParkingLotOwner.java
+++ src/main/java/com/bridgelabzs/ParkingLotOwner.java
@@ -1,10 +1,13 @@
 package com.bridgelabzs;
 
+import java.util.HashMap;
+
 public class ParkingLotOwner implements IParkingLotObserver {
 
     //FIELDS
     boolean isParkingFull;
     ParkingLotSystem parkingLotSystem;
+    private HashMap<Integer, Object> parkingLotMap = new HashMap<Integer, Object>();
 
     @Override
     public void updateParkingLotStatus(boolean parkingLotStatus) {
@@ -15,5 +18,16 @@ public class ParkingLotOwner implements IParkingLotObserver {
         } else
             System.out.println("PARKING LOT OWNER:PARKING_LOT_IS_AVAILABLE");
         parkingLotSystem.isParkingFull = false;
+    }
+
+//    public void getUpdatedMap(HashMap<Integer, Object> parkingLotMap) {
+//        this.parkingLotMap = parkingLotMap;
+//    }
+
+    public Integer generateParkingSlotNumber() {
+        for (int i = 1; i <= 5; i++)
+            if (this.parkingLotMap.get(i) == null)
+                return i;
+        return null;
     }
 }
