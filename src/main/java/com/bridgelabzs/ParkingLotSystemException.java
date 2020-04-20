cd19bdee147f0ebec480419d0d246f66f0b1357b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-20 11:35:36 +0530

    MODIFIED:Proper Indentetion

--- src/main/java/com/bridgelabzs/ParkingLotSystemException.java
+++ src/main/java/com/bridgelabzs/ParkingLotSystemException.java
@@ -2,7 +2,7 @@ package com.bridgelabzs;
 
 public class ParkingLotSystemException extends Exception {
     //ENUM
-    public enum ExceptionType {VEHICLE_NOT_PARK_HERE, PARKING_LOT_IS_FULL};
+    public enum ExceptionType {VEHICLE_NOT_PARK_HERE, PARKING_LOT_IS_FULL}
 
     public ExceptionType type;
     private final String message;
