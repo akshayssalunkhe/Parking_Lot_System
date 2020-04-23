9276ed83fb1add7ea14bac88d86bc104233c9287
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-23 15:12:35 +0530

    ADDED:Class As Per Problem Requirement

--- src/main/java/com/bridgelabzs/PoliceDepartment.java
+++ src/main/java/com/bridgelabzs/PoliceDepartment.java
@@ -1,4 +1,21 @@
 package com.bridgelabzs;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PoliceDepartment {
+    public ParkingLotSystem parkingLotSystem;
+    public Map<String, Vehicle> vehicles;
+
+    //CONSTRUCTOR
+    public PoliceDepartment(ParkingLotSystem parkingLotSystem) {
+        this.parkingLotSystem = parkingLotSystem;
+    }
+
+    //METHOD TO GET VEHICLES OF PARTICULAR COLOUR AND RETUNING MAP
+    public Map<String, Vehicle> getVehicles(Vehicle.VehicleColour colour) {
+        return vehicles = parkingLotSystem.vehicleMap.entrySet().stream()
+                .filter(entry -> colour.equals(entry.getValue().vehicleColour))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
