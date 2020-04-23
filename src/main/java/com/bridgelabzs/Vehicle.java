7e81d23504974fe67c8a16ec27154772b097f595
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-23 16:38:20 +0530

    ADDED:Enum For Vehicle Model

--- src/main/java/com/bridgelabzs/Vehicle.java
+++ src/main/java/com/bridgelabzs/Vehicle.java
@@ -1,21 +1,26 @@
 package com.bridgelabzs;
 
 public class Vehicle {
+    String attendantName;
     VehicleColour vehicleColour;
     VehicleSize vehicleSize;
     DriverType driverType;
     ParkingDateTime parkingDateTime;
     private String parkingDateAndTime;
     private String vehicleId;
+    VehicleModel vehicleModel;
 
     //CONSTRUCTOR
-    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize, VehicleColour vehicleColour) {
+    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize,
+                   VehicleColour vehicleColour,VehicleModel vehicleModel,String attendantName) {
         parkingDateTime = new ParkingDateTime();
         this.vehicleId = vehicleId;
         parkingDateAndTime = parkingDateTime.getDateTime();
         this.driverType = driverType;
         this.vehicleSize = vehicleSize;
         this.vehicleColour = vehicleColour;
+        this.vehicleModel = vehicleModel;
+        this.attendantName = attendantName;
     }
 
     //METHOD TO RETURN PARKING TIME
@@ -34,4 +39,6 @@ public class Vehicle {
     public enum VehicleSize {LARGE, SMALL}
 
     public enum VehicleColour {WHITE, BLUE}
+
+    public enum VehicleModel {BMW, TOYOTA}
 }
